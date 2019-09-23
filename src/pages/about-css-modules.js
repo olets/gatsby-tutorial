eed19eea709475e15a699578ad1eef8c84a8f096
Henry Bley-Vroman <henry.bley-vroman@viget.com>
2019-09-23 18:45:09 -0400

    feat(about css modules): add css module to page

--- src/pages/about-css-modules.js
+++ src/pages/about-css-modules.js
@@ -1,5 +1,8 @@
 import React from "react"
+import styles from "./about-css-modules.module.css"
 import Container from "../components/container"
+console.log(styles)
+
 export default () => (
   <Container>
     <h1>About CSS Modules</h1>
